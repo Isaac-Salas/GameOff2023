92897ff9ce07cabd429c6f30af77d33683c431e6
Isaac Salas <78274906+Isaac-Salas@users.noreply.github.com>
2023-11-11 14:19:26 -0600

    New Pickups! (#43)
    Added:
    -Make MaxCap bigger
    -Make Min Cap smaller
    -Boost the speed at which you can shrink and grow

--- Levels/Sprite.gd
+++ Levels/Sprite.gd
@@ -14,7 +14,8 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
 	rotate_y(deg_to_rad(15))
+
 
 
