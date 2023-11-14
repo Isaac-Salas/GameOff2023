f90eab111adf7c4e1f988fa12d4e21b3f81918ce
Jair Chavez Islas <73390890+Jair0305@users.noreply.github.com>
2023-11-14 12:08:38 -0600

    Added a logic to drop or throw an object to the lastSide that player saw (#74)

--- CharacterBody3D.gd
+++ CharacterBody3D.gd
@@ -1,12 +1,10 @@
 extends CharacterBody3D
 
-
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 
 func _physics_process(delta):
 	# Add the gravity.
