0702257e8053478e724c924d5130e1fa4724f6ce
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2023-11-13 23:03:55 -0600

    Small adjustments

--- level_design_objects/Door.gd
+++ level_design_objects/Door.gd
@@ -2,5 +2,13 @@ extends StaticBody3D
 var isOpen = false
 var inAnimation = false
 @onready var anim = $AnimationPlayer
-func interact():
-	anim.propert
+func _ready():
+	anim.play("Close Door")
+	
+func toggle_door(state, body):
+	isOpen = state
+	if isOpen:
+		anim.play("Open Door")
+	else:
+		anim.play("Close Door")
+	
