0e6d198708c2c3658057c2a7b65932d16de7bfd2
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2023-11-15 15:41:53 -0600

    Version 2 of tutorial area
    added some stuff including a nice red lamp! and a trigger area for jumpscares

--- Player/PlayerTest.gd
+++ Player/PlayerTest.gd
@@ -1,21 +1,36 @@
 extends CharacterBody3D
-
-var pickup
+var pickname
+var pos = get_node(".").position
 var pickupinst = preload("res://Pickups/PickObject.tscn")
-var pickinst = pickupinst.instantiate()
+var pickinst 
+var mundotest
+var objectPicked = null
+var pickup
+var lastSide = "right"
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
+func _input(event):
+	if Input.is_action_just_pressed("Interact"):
+		if objectPicked == true:
+			release_pickup()
+		else:
+			try_pickup()
+	if Input.is_action_just_pressed("Throw"):
+		if objectPicked == true:
+			throw()
+
+
 func _physics_process(delta):
 	#print("Jumpvel:",GlobalVar.JUMP_VELOCITY, "Vel:",GlobalVar.SPEED)
-	position.z == 0
-	var pos = get_node(".").position
+	position.z = 0
+
 	#Updating the label
 	#print(GlobalVar.sizefactor)
 
 	if GlobalVar.sizefactor<=(GlobalVar.MinCap+GlobalVar.MaxCap)/3: 
-		print ((GlobalVar.MinCap+GlobalVar.MaxCap)/3)
+		#print ((GlobalVar.MinCap+GlobalVar.MaxCap)/3)
 		GlobalVar.CURRENT = "SMALL"
 	elif GlobalVar.sizefactor<=(GlobalVar.MaxCap/2)*1.5:
 		GlobalVar.CURRENT = "NORMAL"
@@ -25,7 +40,7 @@ func _physics_process(delta):
 
 
 	#Peruvian Scaling
-	if Input.is_key_pressed(KEY_Z):
+	if Input.is_action_pressed('Item'):
 		#print (scale)
 		
 		#Upscaling until sizefactor <2
@@ -42,12 +57,12 @@ func _physics_process(delta):
 		$GPUParticles3D.emitting = false
 	
 	
-	if Input.is_key_pressed(KEY_X):
-		print(GlobalVar.CURRENT, GlobalVar.sizefactor)
+	if Input.is_action_pressed('Shrink'):
+		#print(GlobalVar.CURRENT, GlobalVar.sizefactor)
 		shrink()
 	
-	if Input.is_key_pressed(KEY_C):
-		print(GlobalVar.CURRENT, GlobalVar.sizefactor)
+	if Input.is_action_pressed('Grow'):
+		#print(GlobalVar.CURRENT, GlobalVar.sizefactor)
 		grow()
 		
 	# Add the gravity.
@@ -55,21 +70,32 @@ func _physics_process(delta):
 		velocity.y -= (gravity*4) * delta
 
 	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		var jumpanim = get_node("MeshInstance3D/Prot-Slime").find_child("AnimationPlayer")
+		jumpanim.play("Armature|Jump Animation")
 		velocity.y = GlobalVar.JUMP_VELOCITY
 	
-	# Speed and Jump velocity tweaks when shrinking or getting big
+	# Speed and Jump velocity tweaks when shrinking or getting bigv
 	if not GlobalVar.CURRENT == "NORMAL" and GlobalVar.sizefactor < 1:
-		GlobalVar.SPEED = snapped(10 / (0.4 + GlobalVar.sizefactor), 1)
-		GlobalVar.JUMP_VELOCITY =snapped( 20 / (0.4 + GlobalVar.sizefactor),1 )
+		GlobalVar.SPEED = snapped(10 / (0.3 + GlobalVar.sizefactor), 1)
+		GlobalVar.JUMP_VELOCITY =snapped( 20 / (0.3 + GlobalVar.sizefactor),1 )
+	else:
+		GlobalVar.SPEED = 10
+		GlobalVar.JUMP_VELOCITY = 20
 
 
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with ass gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var input_dir = Input.get_vector("Walk_Left", "Walk_Right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, input_dir.y,0)).normalized()
 	
+	
+	if direction:
+		if direction.x > 0: 
+			lastSide = "right" 
+		else: 
+			lastSide = "left"
 	#-------------------------------------------------------------------------------------------------------
 	#Perfect example of peruvian solutions c:
 	if direction:
@@ -78,25 +104,36 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, (GlobalVar.SPEED))
 		velocity.z = 0
-		
-	if Input.is_key_pressed(KEY_X) and pickup == true:
-		
-		#print(pos)
+
 	
-		pickinst.transform.origin = (Vector3(0,2,0))
-		pickinst.get_node("Pickup").freeze = true
-		pickinst.get_node("Pickup/Area3D").monitoring = false
-		add_child(pickinst)
-	if Input.is_key_pressed(KEY_X):
-		pickinst.get_node("Pickup").freeze = false
-		
 		
 	move_and_slide()
 
+
+func  throw():
+	mundotest = get_parent_node_3d()
+	$MeshInstance3D.remove_child(pickinst)
+	pickinst.transform.origin = Vector3(0,2,0)
+	if lastSide == "right":		
+		pickinst.get_node("Pickup").linear_velocity = Vector3(20,0,0)
+		pickinst.transform.origin = global_position+Vector3(2,0,0)
+	else:
+		pickinst.get_node("Pickup").linear_velocity = Vector3(-20,0,0)
+		pickinst.transform.origin = global_position+Vector3(-2,0,0)
+	pickinst.get_node("Pickup").freeze = false
+	pickinst.get_node("Pickup/CollisionShape3D").disabled = false
+	pickinst.get_node("Pickup/Area3D").monitoring = true
+	pickup = false
+	objectPicked = false
+	mundotest.add_child(pickinst)
+	
+	
+
 func grow():
 	if GlobalVar.sizefactor < GlobalVar.MaxCap:
-		get_node("MeshInstance3D").scale += Vector3(GlobalVar.Scalerate,GlobalVar.Scalerate,0)
+		get_node("MeshInstance3D").scale += Vector3(GlobalVar.Scalerate,GlobalVar.Scalerate,GlobalVar.Scalerate)
 		get_node("CollisionShape3D").scale += Vector3(GlobalVar.Scalerate,GlobalVar.Scalerate,0)
+		get_node("ObjectDetect").scale += Vector3(GlobalVar.Scalerate,GlobalVar.Scalerate,GlobalVar.Scalerate)
 		get_node("ColisionperuanaDer2").position.x += GlobalVar.Scalerate
 		get_node("ColisionperuanaIzq").position.x -= GlobalVar.Scalerate
 		get_node("ColisionArriba").position.y += GlobalVar.Scalerate
@@ -119,8 +156,9 @@ func grow():
 
 func shrink():
 	if GlobalVar.sizefactor > GlobalVar.MinCap:
-		get_node("MeshInstance3D").scale -= Vector3(GlobalVar.Scalerate,GlobalVar.Scalerate,0)
+		get_node("MeshInstance3D").scale -= Vector3(GlobalVar.Scalerate,GlobalVar.Scalerate,GlobalVar.Scalerate)
 		get_node("CollisionShape3D").scale -= Vector3(GlobalVar.Scalerate,GlobalVar.Scalerate,0)
+		get_node("ObjectDetect").scale -= Vector3(GlobalVar.Scalerate,GlobalVar.Scalerate,GlobalVar.Scalerate)
 		get_node("ColisionperuanaDer2").position.x -= GlobalVar.Scalerate
 		get_node("ColisionperuanaIzq").position.x += GlobalVar.Scalerate
 		get_node("ColisionArriba").position.y -= GlobalVar.Scalerate
@@ -138,15 +176,55 @@ func shrink():
 		$GPUParticles3D.process_material.set("lifetime", 4)
 		$GPUParticles3D.process_material.set_collision_mode(1)
 		$GPUParticles3D.emitting = true
-	
+
+
+func try_pickup():
+	if pickup == true:
+		pickinst =  pickupinst.instantiate()
+		print("espacio del jugador" + str(pos))
+		print("Espacio del objeto" + str(pickinst.transform.origin))
+		print("pickup desde el player")
+		pickinst.transform.origin = Vector3(0,(1.5*GlobalVar.sizefactor),0)
+		pickinst.get_node("Pickup").freeze = true
+		pickinst.get_node("Pickup/CollisionShape3D").disabled = true
+		pickinst.get_node("Pickup/Area3D").monitoring = false
+		pickup = false
+		objectPicked = true
+		$MeshInstance3D.add_child(pickinst)
+		get_parent_node_3d().remove_child(pickname)
+		if mundotest != null:
+			mundotest.remove_child(pickinst)
+		
+
+func release_pickup():
+	print("release desde el player")
+	if objectPicked:
+		mundotest = get_parent_node_3d()
+		$MeshInstance3D.remove_child(pickinst)
+		pickinst.transform.origin = Vector3(0,2,0)
+		pickinst.get_node("Pickup").freeze = false
+		pickinst.get_node("Pickup/CollisionShape3D").disabled = false
+		pickinst.get_node("Pickup/Area3D").monitoring = true
+		pickup = false
+		objectPicked = false
+		if lastSide == "right":
+			pickinst.transform.origin = global_position+Vector3(2,0,0)
+		else:
+			pickinst.transform.origin = global_position+Vector3(-2,0,0)
+		mundotest.add_child(pickinst)
+
+
+
 func _on_object_detect_body_entered(body):
 	if body.name == "Pickup":
+		pickname = body.get_parent_node_3d()
+		print (pickname.name)
 		print("Assin")
 		pickup = true
 
-
-
 func _on_object_detect_body_exited(body):
 	if body.name == "Pickup":
+		pickname = body.get_parent_node_3d()
+		print (pickname.name)
 		print("Assout")
 		pickup = false
--- level_design_objects/Area_Trigger.gd
+++ level_design_objects/Area_Trigger.gd
@@ -7,3 +7,5 @@ signal entered(state, body)
 func _on_body_entered(body):
 	if body.name == "Player":
 		emit_signal("entered", false, body)
+		queue_free()
+	
