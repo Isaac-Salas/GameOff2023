3c91c2aa7582ddbc43ed10219437b8e326cb2b80
Alejandro Alonso Sánchez <49330757+alexcavadora@users.noreply.github.com>
2023-11-28 11:33:26 -0600

    Dialog2 (#135)

--- NPCs/SimpleCube.gd
+++ NPCs/SimpleCube.gd
@@ -1,36 +1,50 @@
 extends RigidBody3D
-var bump = false
-var thing
-var playpos
+
+var state = false
+const lines: Array[String] =[
+	"Hey, you seem pretty strong!",
+	"Wanna spar?",
+	"Wait...",
+	"I shouldn't waste my energy before an important battle...",
+	"Well, I'll see you at the buffet!",]
+
+func _unhandled_input(event):
+	if state and event.is_action_pressed("Interact"):
+		DialogManager.start_dialog(global_position, lines)
+		state = false
+	
+func _on_area_3d_body_entered(body):
+	if body.name == "Player":
+		state = true
+
+func _on_area_3d_body_exited(body):
+	state = false # Replace with function body.
+
+#var bump = false
+#var thing
+#var playpos
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	set_linear_velocity(get_linear_velocity()+Vector3(0,0,0))
+#func _ready():
+#	set_linear_velocity(get_linear_velocity()+Vector3(0,0,0))
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+#func _process(delta):
 		#Peruvian collisions
-	if bump == true:
-		pass
+#	if bump == true:
+#		pass
 #		if playpos.x > 0 and playpos.y > 0 and playpos.y < 1.2 :
 #			set_linear_velocity(Vector3(-10,0,0))
 #		if playpos.x < 0 and playpos.y > 0 and playpos.y < 1.2 :
 #			set_linear_velocity(Vector3(10,0,0))
+		#bump = true
+		#thing = body
+		#return body
 
 
-		
+#func _on_area_3d_body_exited(body):
+#	if body.name == "Player":
+#		bump = false
+#		thing = body
+#		return body
 
-func _on_area_3d_body_entered(body):
-	playpos = (body.global_position)-(get_node("Area3D").global_position)
-	if body.name == "Player":
-		print(playpos)
-		bump = true
-		thing = body
-		return body
-
-
-func _on_area_3d_body_exited(body):
-	if body.name == "Player":
-		bump = false
-		thing = body
-		return body
