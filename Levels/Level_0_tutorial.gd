dba1a5045a62ab0a6e2c4366d1debd2f776541a6
alexcavadora <49330757+alexcavadora@users.noreply.github.com>
2023-11-27 19:22:33 -0600

    Squashed bugs on scale and laser
    Laser keeps the player min and max static on tutorial level but works  well when freeing character control over its scale.

--- Levels/Level_0_tutorial.gd
+++ Levels/Level_0_tutorial.gd
@@ -1,8 +1,17 @@
 extends Node3D
+@export var min_cap = 1.0
+@export var max_cap = 1.0
+@export var size_factor = 1.0
+@export var size_standard = 1.0
 
 func _ready():
-	GlobalVar.MinCap = 1
-	GlobalVar.MaxCap = 1
-	GlobalVar.sizefactor = 1
-	GlobalVar.sizestandard = 1
-	$Player/RigidBody3D.mass = 1
+	GlobalVar.MinCap = min_cap
+	GlobalVar.MaxCap = max_cap
+	GlobalVar.sizefactor = size_factor
+	GlobalVar.sizestandard = size_standard
+	$Player/RigidBody3D.mass = size_standard
+
+func _process(_delta):
+	GlobalVar.MinCap = $Player.scale.x
+	GlobalVar.MaxCap = $Player.scale.x
+	
