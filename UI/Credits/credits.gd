0d541e02d2de340a5e92382bebf4df5f07152f6f
Isaac Salas <iscprepa@gmail.com>
2023-12-01 13:26:30 -0600

    Literally me dude

--- UI/Credits/credits.gd
+++ UI/Credits/credits.gd
@@ -3,3 +3,7 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Label/AnimationPlayer.play("Credits")
+
+
+func _on_texture_button_2_pressed():
+	get_tree().change_scene_to_file("res://Menu/Title-Screen.tscn")
