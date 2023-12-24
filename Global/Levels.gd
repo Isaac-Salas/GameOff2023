8ac1c5e4c8bf3f646ad92e3cda7700ccf2aa8180
Isaac Salas <iscprepa@gmail.com>
2023-12-23 20:39:29 -0600

    Yes

--- Global/Levels.gd
+++ Global/Levels.gd
@@ -1,8 +1,7 @@
 extends Node
 const SAVE_FILE = "res://Saves/save_file.save"
-var g_data = {}
 
-var Level1 = true
+var Level1 = false
 var Level2 = false
 var Level3 = false
 var Level4 = false
@@ -23,3 +22,18 @@ func save():
 func load_data():
 	if FileAccess.file_exists(SAVE_FILE):
 		var file = FileAccess.open(SAVE_FILE, FileAccess.READ)
+		Level1 = file.get_var(Level1)
+		Level2 = file.get_var(Level2)
+		Level3 = file.get_var(Level3)
+		Level4 = file.get_var(Level4)
+		Level5 = file.get_var(Level5)
+		Level6 = file.get_var(Level6)
+	else:
+		print("No data saved...")
+		Level1 = false
+		Level2 = false
+		Level3 = false
+		Level4 = false
+		Level5 = false
+		Level6 = false
+		Level7 = false
