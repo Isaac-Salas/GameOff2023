8cea7bfb3d4d470765e526a059ac0d16354d2861
Isaac Salas <iscprepa@gmail.com>
2024-01-24 19:33:44 -0600

    Save System tweaks

--- Global/Levels.gd
+++ Global/Levels.gd
@@ -1,6 +1,8 @@
 extends Node
 const SAVE_FILE = "res://Saves/save_file.save"
 
+var Firsttime = true
+
 var Level1 = false
 var Level2 = false
 var Cutscene1 = false
@@ -10,8 +12,10 @@ var Level5 = false
 var Level6 = false
 var Level7 = false
 
+
 func save():
 	var file = FileAccess.open(SAVE_FILE, FileAccess.WRITE)
+	file.store_var(Firsttime)
 	file.store_var(Level1)
 	file.store_var(Level2)
 	file.store_var(Level3)
@@ -23,6 +27,7 @@ func save():
 func load_data():
 	if FileAccess.file_exists(SAVE_FILE):
 		var file = FileAccess.open(SAVE_FILE, FileAccess.READ)
+		Firsttime = file.get_var(Firsttime)
 		Level1 = file.get_var(Level1)
 		Level2 = file.get_var(Level2)
 		Level3 = file.get_var(Level3)
@@ -31,6 +36,7 @@ func load_data():
 		Level6 = file.get_var(Level6)
 	else:
 		print("No data saved...")
+		Firsttime = true
 		Level1 = false
 		Level2 = false
 		Level3 = false
