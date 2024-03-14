a0eeacd3888b63c2d26b59a6fa7fa36ea11d030e
Isaac Salas <78274906+Isaac-Salas@users.noreply.github.com>
2024-03-14 13:34:45 -0600

    Isaac/fix (#159)
    Yes

--- level_design_objects/Grow-Ray/Grow-Ray.gd
+++ level_design_objects/Grow-Ray/Grow-Ray.gd
@@ -7,6 +7,12 @@ signal shrink_player(target_size)
 func _process(_delta):
 	if shrink_ray.is_colliding():
 		var collider = shrink_ray.get_collider()
-		if collider.name == 'Player':
-			emit_signal("shrink_player", target_size)
+	if shrink_ray.is_colliding():
+		var collider = shrink_ray.get_collider()
+		match collider.name:
+			'Player':
+				$"../Player"._on_ray_collided(target_size)
+				GlobalVar.MinCap = target_size
+				GlobalVar.MaxCap = target_size
+				GlobalVar.sizefactor = target_size
 			
